949d46639bc3504e7384d8e1283ad58c66178627
Tina Ostrander <tostrander@greenriver.edu>
2020-01-06 14:08:39 -0500

    Created index file

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: laptop
+ * Date: 1/6/2020
+ * Time: 10:51 AM
+ */
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>New Project</h1>
+</body>
+</html>
